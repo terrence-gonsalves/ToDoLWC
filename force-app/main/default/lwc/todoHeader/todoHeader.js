6914cbc34dfacdba02eddee23921c45038afce71
terrence.gonsalves <terrence.gonsalves@gmail.com>
2025-11-16 11:19:17 -0500

    updated title to be configurable

--- force-app/main/default/lwc/todoHeader/todoHeader.js
+++ force-app/main/default/lwc/todoHeader/todoHeader.js
@@ -1,3 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class TodoHeader extends LightningElement {}
+export default class TodoHeader extends LightningElement {
+    @api title = 'Task Trackr';
+}
