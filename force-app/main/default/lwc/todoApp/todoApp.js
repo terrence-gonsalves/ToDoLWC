13fabba53401df4d9b906a7a11bc1de49aa0a39e
terrence.gonsalves <terrence.gonsalves@gmail.com>
2025-12-10 10:36:54 -0500

    JS file with initial imports set

--- force-app/main/default/lwc/todoApp/todoApp.js
+++ force-app/main/default/lwc/todoApp/todoApp.js
@@ -1,3 +1,11 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { refreshApex } from '@salesforce/apex';
+
+import getTodoItems from '@salesforce/apex/TodoItemController.getTodoItems';
+import createTodoItem from '@salesforce/apex/TodoItemController.createTodoItem';
+import updateTodoItem from '@salesforce/apex/TodoItemController.updateTodoItem';
+import deleteTodoItem from '@salesforce/apex/TodoItemController.deleteTodoItem';
+import updateCompletionStatus from '@salesforce/apex/TodoItemController.updateCompletionStatus';
 
 export default class TodoApp extends LightningElement {}
